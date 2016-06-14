b27a6924be7d0ae2533a5dc1e9c7f1f7c1fc9837
zhangmengqi <542018543@qq.com>
2016-06-14 11:33:24 +0800

    修正sql错误

--- user.php
+++ user.php
@@ -151,7 +151,7 @@ else if($aciton = 'register_teacher'){
     $extend_info_list = $db->getAll($sql);
     $smarty->assign('extend_info_list', $extend_info_list);
 
-    $sql = 'SELECT * FROM ' . $ecs->table('course') . ' ORDER BY dis_order, id';
+    $sql = 'SELECT * FROM ' . $ecs->table('course') . ' ORDER BY id';
     /* 取出课程相关信息 */
     $course_info = $db->getAll($sql);
     $smarty->assign('course_id',$course_info);
