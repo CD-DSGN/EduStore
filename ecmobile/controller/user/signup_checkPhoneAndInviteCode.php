3b4101022af380ac25263b605166ebc4e3e5dd4c
evolutionn <779268792@qq.com>
2016-12-20 15:53:20 +0800

    android邀请码
    Signed-off-by: evolutionn <779268792@qq.com>

--- ecmobile/controller/user/signup_checkPhoneAndInviteCode.php
+++ ecmobile/controller/user/signup_checkPhoneAndInviteCode.php
@@ -6,17 +6,29 @@
 include_once(EC_PATH . '/sms/CCPRestSmsSDK.php');
 require(EC_PATH . '/includes/init.php');
 require(EC_PATH . '/sms/SendSmsForVerificationCode.php');
+
 $phone_num = _POST('phonenum');
+$invite_code = isset($_POST['invite_code']) ? addslashes(trim($_POST['invite_code'])): 0;
+
 $time='5';//验证码有效时间
+
 if ($_CFG['shop_reg_closed']) {
     GZ_Api::outPut(10);
 }
-
-$sql = 'SELECT user_id FROM '.$ecs->table('users').' WHERE mobile_phone = '.$phone_num;
-$user_id_all=$db->getAll($sql);
+if(isset($phone_num)){
+    $sql = 'SELECT user_id FROM '.$ecs->table('users').' WHERE mobile_phone = '.$phone_num;
+    $user_id_all=$db->getAll($sql);
+}
+if(isset($invite_code) && $invite_code != 0){
+    $sql = "SELECT recommend_code FROM" . $ecs->table('teachers') . "WHERE recommend_code = '" . $invite_code . "' ";
+    $res = $db->query($sql);
+    $is_exist = mysql_num_rows($res);
+}
 
 if(!empty($user_id_all)){
     GZ_Api::outPut(12);
+}else if($invite_code != 0 && $is_exist == 0){
+    GZ_Api::outPut(15);
 }else{
     $sendSms = new SendSmsForVerificationCode($phone_num, $time);
     $sendSms->sendSmsForVeriCode();
