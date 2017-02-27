466783368eb62d61d0d110a7fcdfdd8d6b5c3345
huan-jie <nahuanjie@gmail.com>
2017-02-27 10:57:19 +0800

    nhj:教师注册及会员地区搜索
    1、教师在注册静态刷新时，如果ajax返回失败，则增加提示；
    2、增加管理中心-会员列表中的省市县查询。

--- admin/users.php
+++ admin/users.php
@@ -693,6 +693,12 @@ function user_list()
         $filter['sort_by']    = empty($_REQUEST['sort_by'])    ? 'teacher_integral' : trim($_REQUEST['sort_by']);
         $filter['sort_order'] = empty($_REQUEST['sort_order']) ? 'DESC'     : trim($_REQUEST['sort_order']);
 
+        /*增加省市地区查询*/
+        $filter['province'] = empty($_REQUEST['province_id'])  ?  0 : trim($_REQUEST['province_id']);
+        $filter['city'] = empty($_REQUEST['city_id'])  ? 0 : trim($_REQUEST['city_id']);
+        $filter['district'] = empty($_REQUEST['town_id'])  ? 0 : trim($_REQUEST['town_id']);
+        /*end*/
+
         $ex_where = ' WHERE 1 ';
         if ($filter['keywords'])
         {
@@ -741,6 +747,20 @@ function user_list()
             }
         }
         /*end*/
+        /*增加省市地区的判断*/
+        if ($filter['users'] == 1) {        // 教师才有地区信息
+            if ($filter['province']) {
+                $region_where = "WHERE `province` = " .$filter['province'];
+            }
+            if ($filter['city']) {
+                $region_where .= " AND `city` = " .$filter['city'];
+            }
+            if ($filter['town']) {
+                $region_where .= " AND 'district' = " .$filter['town'];
+            }
+            $ex_where .= " AND `user_id` in (SELECT user_id FROM ". $GLOBALS['ecs']->table('teachers') . $region_where .")";
+        }
+        /*end*/
         $filter['record_count'] = $GLOBALS['db']->getOne("SELECT COUNT(*) FROM " . $GLOBALS['ecs']->table('users') . $ex_where);
 
         /* 分页大小 */
@@ -749,7 +769,6 @@ function user_list()
                 " FROM " . $GLOBALS['ecs']->table('users') . $ex_where .
                 " ORDER by " . $filter['sort_by'] . ' ' . $filter['sort_order'] .
                 " LIMIT " . $filter['start'] . ',' . $filter['page_size'];
-
         $filter['keywords'] = stripslashes($filter['keywords']);
         set_filter($filter, $sql);
     }
