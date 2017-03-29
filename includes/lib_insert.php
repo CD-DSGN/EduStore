fd1028728321c97802e6983be6c8bfa36f21af78
evolutionn <779268792@qq.com>
2017-03-29 18:48:35 +0800

    防sql注入
    Signed-off-by: evolutionn <779268792@qq.com>

--- includes/lib_insert.php
+++ includes/lib_insert.php
@@ -137,6 +137,8 @@ function insert_cart_info()
  */
 function insert_ads($arr)
 {
+    $arr['num'] = intval($arr['num']);
+    $arr['id'] = intval($arr['id']);
     static $static_res = NULL;
 
     $time = gmtime();
@@ -267,6 +269,8 @@ function insert_comments($arr)
     $need_cache = $GLOBALS['smarty']->caching;
     $need_compile = $GLOBALS['smarty']->force_compile;
 
+    $arr['id'] = intval($arr['id']);
+    $arr['type'] = addslashes($arr['type']);
     $GLOBALS['smarty']->caching = false;
     $GLOBALS['smarty']->force_compile = true;
 
@@ -302,6 +306,7 @@ function insert_comments($arr)
  */
 function insert_bought_notes($arr)
 {
+    $arr['id'] = intval($arr['id']);
     $need_cache = $GLOBALS['smarty']->caching;
     $need_compile = $GLOBALS['smarty']->force_compile;
 
