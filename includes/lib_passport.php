4d60e19841b1acbb1c8bc76de12945495adc3c1a
zhangmengqi <542018543@qq.com>
2017-05-16 10:35:54 +0800

    漏传

--- includes/lib_passport.php
+++ includes/lib_passport.php
@@ -162,7 +162,7 @@ function register($username, $password, $email, $other = array(), $invite_code =
         }
 
         //定义other合法的变量数组
-        $other_key_array = array('msn', 'qq', 'office_phone', 'home_phone', 'mobile_phone');
+        $other_key_array = array('msn', 'qq', 'office_phone', 'home_phone', 'mobile_phone', 'nickname');
         $update_data['reg_time'] = local_strtotime(local_date('Y-m-d H:i:s'));
         if ($other)
         {
