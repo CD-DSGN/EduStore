a54fe6e2a74bf951bd095ed465965088af561e47
huan-jie <nahuanjie@gmail.com>
2017-06-09 14:36:03 +0800

    那奂捷：iOS汇师圈评论成功返回评论内容

--- ecmobile-ios/controller/interaction/comment_publish.php
+++ ecmobile-ios/controller/interaction/comment_publish.php
@@ -27,7 +27,84 @@ $sql = 'INSERT INTO ' .$ecs->table('huishi_circle_comment') ." SET
         .$sql_target_comment_id;
 $res = $db->query($sql);
 if($res == true){
-    GZ_Api::outPut(array());
+    
+	$comment = get_teacher_publish_comments($news_id);
+	// var_dump($comment);
+    GZ_Api::outPut(array('commentInfo' => $comment));
+
 }else{
     GZ_Api::outPut(708);
+}
+
+function get_teacher_publish_comments($news_id){
+    $sql = "SELECT * FROM ". $GLOBALS['ecs']->table('huishi_circle_comment') ." WHERE `news_id` = $news_id ORDER BY publish_time ASC";
+    $comment_info = $GLOBALS['db']->getAll($sql);
+    $comments = array();
+    foreach ($comment_info as $key =>$value){
+        $uid = get_user_id_by_comment_id($value['target_comment_id']);
+        $target_username = get_username_by_user_id($uid);
+        if($target_username == null){
+            $target_username = '';
+        }
+
+        $show_target_name = get_showname_by_user_id($uid);
+        $show_name = get_showname_by_user_id($value['user_id']);
+
+        $comments[] = array(
+            'comment_id' => $value['comment_id'],
+            'username' => get_username_by_user_id($value['user_id']),
+            'target_username' => $target_username,
+            'comment_content' => $value['comment_content'],
+            'show_name' =>  $show_name,
+            'show_target_name' =>$show_target_name,
+            // add nhj, 在这里同时返回news_id
+            'news_id' => $news_id);
+    }
+    return $comments;
+}
+
+function get_username_by_user_id($uid){
+    $sql = "SELECT user_name FROM ". $GLOBALS['ecs']->table('users') ."WHERE `user_id` = '". $uid ."'";
+    $res = $GLOBALS['db']->getRow($sql);
+    $user_name = $res['user_name'];
+    return $user_name;
+}
+function get_user_id_by_comment_id($comment_id){
+    $sql = "SELECT user_id FROM ". $GLOBALS['ecs']->table('huishi_circle_comment') ."WHERE `comment_id` = '". $comment_id ."'";
+    $res = $GLOBALS['db']->getOne($sql);
+    return $res;
+}
+function get_teacher_name_by_user_id($uid){
+    $sql = "SELECT real_name FROM ". $GLOBALS['ecs']->table('teachers') ."WHERE `user_id` = '". $uid ."'";
+    $res = $GLOBALS['db']->getRow($sql);
+    $teacher_name = $res['real_name'];
+    return $teacher_name;
+}
+
+
+function get_nickname_by_user_id($uid)
+{
+    $sql = "SELECT nickname FROM ". $GLOBALS['ecs']->table('users') ."WHERE `user_id` = '". $uid ."'";
+    $res = $GLOBALS['db']->getone($sql);
+    return $res;
+}
+
+function get_showname_by_user_id($uid)
+{
+    $is_teacher = judge_user_status($uid);
+    if ($is_teacher) {
+        $name = get_teacher_name_by_user_id($uid);
+    }else{
+        $name = get_nickname_by_user_id($uid);
+    }
+    return $name;
+}
+
+function judge_user_status($user_id)
+{
+	$sql = "SELECT is_teacher FROM ". $GLOBALS['ecs']->table('users') ." WHERE `user_id` = '". $user_id ."'";
+	$row = $GLOBALS['db']->getRow($sql);
+	$is_teacher = $row['is_teacher'];
+
+	return $is_teacher;
 }
